3008b5736786be686db85f7339c66132b60e624a
VasupriyaPatnaik <vasupriyab004@gmail.com>
2026-04-10 00:32:13 +0530

    fix: Update API_BASE to use environment variable for better configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://127.0.0.1:8000/api";
 
 const defaultTokens = {
   colors: {
